366dd4191dbc8cd62d6bce425658cfbd93d69f20
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2026-07-19 17:18:13 +0400

    Complete the CoffeeShop website, full css, js animations

--- CoffeeShopWebsite/script.js
+++ CoffeeShopWebsite/script.js
@@ -1,1 +1,45 @@
-// Script js
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+})
+
+// Testimonial Sldier
+const reviews = document.querySelectorAll(".review");
+let current = 0;
+
+function changeReview () {
+  reviews[current].classList.remove("active");
+  current = (current + 1) % reviews.length;
+  reviews[current].classList.add("active");
+}
+
+setInterval(changeReview, 3000);
+
+// Counter Animation
+const counters = document.querySelectorAll(".counter");
+
+const animateCounter = () => {
+  counters.forEach(counter => {
+    const target = +counter.dataset.target;
+
+    let count = 0;
+    const increment = target / 100;
+
+    const update = () => {
+      count += increment;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count);
+        requestAnimationFrame(update);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    update();
+  });
+};
+
+window.addEventListener("load", animateCounter);
